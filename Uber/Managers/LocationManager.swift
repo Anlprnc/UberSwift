9e00724dc31d04fda48a25f10afe748833a2798a
Anlprnc <anilpirincciii@gmail.com>
2024-09-13 16:24:36 +0300

    map

--- Uber/Managers/LocationManager.swift
+++ Uber/Managers/LocationManager.swift
@@ -5,4 +5,23 @@
 //  Created by Anıl on 13.09.2024.
 //
 
-import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        locationManager.stopUpdatingLocation()
+    }
+}
